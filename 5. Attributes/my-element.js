b14ff6fd10700e503b4057e60a6b52084579ffdc
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2022-01-15 11:02:41 -0400

    attribute

--- 5. Attributes/my-element.js
+++ 5. Attributes/my-element.js
@@ -2,7 +2,12 @@ class myElement extends HTMLElement {
     // En el constructor estamos creando todo lo que se vaya a guardar en memoria, Para que despues podamos agregarlos como nodos al DOM
     constructor() {
         super();
-        this.attachShadow({ mode: "open" }) 
+        this.attachShadow({ mode: "open" });
+
+        // Ponemos los atributos en el contructor
+        this.title = this.getAttribute("title");
+        this.paragraph = this.getAttribute("paragraph");
+        this.img = this.getAttribute("img");
     }
     // Genero directamente el tag template
     getTemplate() {
@@ -10,14 +15,11 @@ class myElement extends HTMLElement {
         const template = document.createElement("template");
         template.innerHTML =/*html */ `
             <section>
-                <h2>
-                <!---En el slot tendremos el texto que tenemos en la etiqueta de my element--->
-                    <slot name="title"></slot>
-                </h2>
+                <!-- Inyectamos los atributos -->
+                <h2>${this.title}</h2>
                 <div>
-                    <p>
-                        <slot name="paragraph"></slot>
-                    </p>
+                    <p>${this.paragraph}</p>
+                    <img src=${this.img}/>
                 </div>
             </section>
             ${this.getStyle()}
